b5ecee5e566b05eb49d2912c56e88b73b4a7b5a5
Vaagish Kumar Singh <vaagish1124@gmail.com>
2025-08-16 11:39:00 +0000

    Counting the length of the last word in a sentence.

---
+++ lengthOfLastWord.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+ int lengthOfLastWord(string s) {
+         int length = 0;
+        int i = s.size() - 1;
+        
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+        
+        // Count the length of the last word
+        while (i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
+        }
+        
+        return length;
+    }
+
+int main()
+{
+    string sentence = "Hello World";
+    cout<<lengthOfLastWord(sentence)<<endl;
+    
+    return 0;
+}
